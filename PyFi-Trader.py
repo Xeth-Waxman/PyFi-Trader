a7c4528ecb8b3f9c80ab772eb1c1094790ebd75c
Xeth Waxman <xwaxman@gmail.com>
2019-12-28 16:37:40 -0500

    Continued Build-out
    Continued playing around with Pandas dataframe and MatPlotLib to investigate different charts and visualizations

--- PyFi-Trader.py
+++ PyFi-Trader.py
@@ -11,6 +11,9 @@ style.use('ggplot')
 start_date = dt.datetime(2000, 1, 1)
 end_date = dt.datetime(2019, 12, 27)
 
-# put the symbol's data into a data frame, getting the data from yahoo
+# put the symbol's data into a data frame, getting the data from yahoo's API
 df = web.DataReader('LPL', 'yahoo', start_date, end_date)
-print(df.head())
+
+# what does this look like?
+df['Adj Close'].plot()
+pyplot.show()
