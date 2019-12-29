a4a7c280a207fb3fd6e5a654135714ce41284f22
Xeth Waxman <xwaxman@gmail.com>
2019-12-28 19:35:39 -0500

    Data Munging Pt 1
    Some light refactoring. Added 100 day MA to the data frame, played around with visualizing it.

--- PyFi-Trader.py
+++ PyFi-Trader.py
@@ -1,5 +1,5 @@
 import datetime as dt
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
 from matplotlib import style
 import pandas as panda
 import pandas_datareader.data as web
@@ -14,6 +14,15 @@ end_date = dt.datetime(2019, 12, 27)
 # put the symbol's data into a data frame, getting the data from yahoo's API
 df = web.DataReader('LPL', 'yahoo', start_date, end_date)
 
-# what does this look like?
-df['Adj Close'].plot()
-pyplot.show()
+# let's add a 100 day moving average
+df['100ma'] = df['Adj Close'].rolling(window = 100, min_periods=0).mean()
+
+# set up our axiseseses
+ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=5, colspan=5)
+ax2 = plt.subplot2grid((6, 1), (5, 0), rowspan=1, colspan=1, sharex = ax1)
+
+# le plot it
+ax1.plot(df.index, df['Adj Close'])
+ax1.plot(df.index, df['100ma'])
+ax2.bar(df.index, df['Volume'])
+plt.show()
