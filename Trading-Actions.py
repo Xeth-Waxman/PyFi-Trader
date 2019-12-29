21836c7d5b4f14ed0653c0a650754d537c2c001e
Xeth Waxman <xwaxman@gmail.com>
2019-12-29 17:01:20 -0500

    Trading-Actions Buildout
    Started defining the trading features (Buy, Sell, Hold) by creating a dataframe label that holds the percentage movement of a stock over a specified period of time

---
+++ Trading-Actions.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+import pickle
+
+def process_labels(ticker, lag_days = 7):
+    """
+    This function takes a given stock symbol and a lag-time, specified in days, and creates a data-frame
+    loaded from 'SP500_formatted.csv', which is created by executing PyFi-Playground. The data-frame will contain
+    the original data from SP500_formatted.csv, plus a new set of columns that take the format
+    {}_{}d.format(ticker, j), where j is between 1 and lag_days. This column will contain the percentage change
+    of the price vs the price j days into the future; e.g., AAPL_1d of -1.00 means the price of Apple will decline
+    by 1% the following day
+    :param ticker: The stock symbol to be labeled. Must be in the S&P 500 and pre-loaded into SP500_formatted.csv
+    :param lag_days: The number of days into the future to evaluate. The default is 7
+    :return: tickers, a list of the values of the SP500_formatted.csv file, and df - a dataframe labeled with the
+    percentage change of the passed ticker
+    """
+    lag_days += 1
+    df = pd.read_csv('SP500_formatted.csv')
+    tickers = df.columns.values.tolist()
+    df.fillna(0, inplace=True)
+
+    for i in range(1, lag_days):
+        df['{}_{}d'.format(ticker, i)] = (df[ticker].shift(-i) - df[ticker]) / df[ticker]
+
+    df.fillna(0, inplace=True)
+    return tickers, df
