2dfdac8c69ae3098b615623716b1f8e18bee9cd6
Xeth Waxman <xwaxman@gmail.com>
2019-12-28 20:35:15 -0500

    More Symbols Please
    Now that I have an idea of the chart types to work with, I'm starting fresh to start figuring out how to scrape the entire S&P 500 ticker list. This way we're not restricted to a single stock when trading

--- PyFi-Trader.py
+++ PyFi-Trader.py
@@ -1,41 +1,25 @@
-import datetime as dt
-import matplotlib.pyplot as plt
-from matplotlib import style
-from mpl_finance import candlestick_ohlc
-import matplotlib.dates as mdates
-import pandas as panda
-import pandas_datareader.data as web
-from pandas.plotting import register_matplotlib_converters
+import bs4 as bs
+import pickle
+import requests
 
-# this addresses the future warning about 'implicitly registered datetime converters'
-register_matplotlib_converters()
 
-# we'll use ggplot as our charting style
-style.use('ggplot')
+def save_sp500_tickers():
+    """
+    gets a list of the current S&P 500 stock symbols and downloads them from https://www.slickcharts.com/sp500
+    :return:
+    tickers object of 500 stock symbols comprising the Standards & Poor 500 Index
+    """
+    response = requests.get('https://www.slickcharts.com/sp500')
+    soup = bs.BeautifulSoup(response.text, 'lxml')
+    table = soup.find('table', {'class': 'table table-hover table-borderless table-sm'})
+    tickers = []
+    for row in table.findAll('tr')[1:]:
+        ticker = row.findAll('td')[2].text
+        tickers.append(ticker)
 
-# let's create a date range - Jan 1 2000 - Dec 27 2019 (yesterday)
-start_date = dt.datetime(2000, 1, 1)
-end_date = dt.datetime(2019, 12, 27)
+    with open("sp500_tickers.pickle", "wb") as f:
+        pickle.dump(tickers, f)
 
-# put the symbol's data into a data frame, getting the data from yahoo's API
-df = web.DataReader('LPL', 'yahoo', start_date, end_date)
+    return tickers
 
-# re-sample the data into 10-day periods
-df_ohlc = df['Adj Close'].resample('10D').ohlc()
-df_volume = df['Volume'].resample('10D').sum()
-
-# weird date-fu because matplotlib
-df_ohlc.reset_index(inplace=True)
-df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
-
-# set up our axiseseses
-ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=5, colspan=5)
-ax2 = plt.subplot2grid((6, 1), (5, 0), rowspan=1, colspan=1, sharex = ax1)
-ax1.xaxis_date()
-
-# candlestick that shizz
-candlestick_ohlc(ax1, df_ohlc.values, width=2, colorup='g')
-ax2.fill_between(df_volume.index.map(mdates.date2num), df_volume.values, 0)
-
-# show that sh**
-plt.show()
+print(save_sp500_tickers())
