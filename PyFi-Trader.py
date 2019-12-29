c5131ce304311a0ff9d971acfbc81b045a5433a2
Xeth Waxman <xwaxman@gmail.com>
2019-12-28 21:16:08 -0500

    Get 'em all
    Created a loop which downloads all S&P 500 tickers from Yahoo and stores them locally so we don't have to do that all the time.

--- PyFi-Trader.py
+++ PyFi-Trader.py
@@ -1,7 +1,10 @@
 import bs4 as bs
 import pickle
 import requests
-
+import datetime as dt
+import os
+import pandas as pd
+import pandas_datareader.data as web
 
 def save_sp500_tickers():
     """
@@ -22,4 +25,30 @@ def save_sp500_tickers():
 
     return tickers
 
-print(save_sp500_tickers())
+def get_data_from_yahoo(reloadSP500=False):
+    """
+    gets pricing data from yahoo for a stock ticker. If reload is true, the function will refresh
+    the ticker list. Use this if you suspect the SP500 index has recently changed.
+    :return:
+    """
+    if reloadSP500:
+        tickers = save_sp500_tickers()
+    else:
+        with open("sp500_tickers.pickle", "rb") as f:
+            tickers = pickle.load(f)
+
+    if not os.path.exists('stock_dfs'):
+        os.makedirs('stock_dfs')
+
+    start = dt.datetime(2000, 1, 1)
+    end = dt.datetime(2019, 12, 31)
+
+    for ticker in tickers:
+        #replace '.' with '-', because Yahoo
+        ticker = ticker.replace(".", "-")
+        if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+            df = web.DataReader(ticker, 'yahoo', start, end)
+            df.to_csv('stock_dfs/{}.csv'.format(ticker))
+            print("processing {}".format(ticker))
+
+get_data_from_yahoo(True)
